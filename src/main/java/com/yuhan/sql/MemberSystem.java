00f70ade32f40a80320a5633227d9c30fbb5d29a
jaekwan <aries278@naver.com>
2021-10-06 16:40:39 +0900

    MemberSystem

---
+++ src/main/java/com/yuhan/sql/MemberSystem.java
@@ -0,0 +1,45 @@
+package com.yuhan.sql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import com.sns.table.Member;
+
+public class MemberSystem {
+	
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("sns");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getMember(1);
+	}
+	
+	
+	public static void getMember(int seq) {
+		EntityManager em = emf.createEntityManager();
+		String query = "SELECT c FROM Member c WHERE c.seq = :seq";
+		
+		TypedQuery<Member> tq = em.createQuery(query, Member.class);
+		
+		tq.setParameter("seq", seq);
+		
+		Member member = null;
+		
+		try {
+			member = tq.getSingleResult();
+			System.out.println(member.getSeq() + " " + member.getId() + " " + member.getPassword() + 
+								" " + member.getName() + " " + member.getTel() + " " + member.getEmail()
+								+ " " + member.getGender() + " " + member.getAge());
+		} catch(NoResultException e){
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+	}
+	
+}
+
+
